1daa391769b2036f6f8097ce963153232ddd0b2b
Bogac Kucuk <bogackucuk@outlook.com>
2023-08-19 22:38:19 +0300

    component index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,8 @@
+import Button from "./Button";
 import Nav from "./Nav";
+import ShoeCard from "./ShoeCard";
+import PopularProductCard from "./PopularProductCard";
+import ServiceCard from "./ServiceCard";
+import ReviewCard from "./ReviewCard";
 
-export { Nav };
+export { Button, Nav, ShoeCard, PopularProductCard, ServiceCard, ReviewCard };
