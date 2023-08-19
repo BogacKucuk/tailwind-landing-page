acb8edf41d6f71dceb7147e0a7b385b53a844628
Bogac Kucuk <bogackucuk@outlook.com>
2023-08-19 22:27:37 +0300

    App adjusments

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Nav } from "./components";
 import {
   CustomerReviews,
   Footer,
@@ -10,17 +11,35 @@ import {
 } from "./sections";
 
 const App = () => {
-  <main className="relative">
-    Nav
-    <section className="xl:padding-1 wide:padding-r padding-b">Hero</section>
-    <section className="padding">Popular Products</section>
-    <section className="padding">Super Quality</section>
-    <section className="padding-x py-10">Services</section>
-    <section className="padding bg-pale-blue">Special Offer</section>
-    <section className="padding">Customer Reviews</section>
-    <section className="padding-x sm:py-32 py-16 w-full">Subscribe</section>
-    <section className="bg-black padding-x padding-t pb-8">Footer</section>
-  </main>;
+  return (
+    <main className="relative">
+      <Nav />
+      <section className="xl:padding-l wide:padding-r padding-b">
+        <Hero />
+      </section>
+      <section className="padding">
+        <PopularProducts />
+      </section>
+      <section className="padding">
+        <SuperQuality />
+      </section>
+      <section className="padding-x py-10">
+        <Services />
+      </section>
+      <section className="padding">
+        <SpecialOffer />
+      </section>
+      <section className="bg-pale-blue padding">
+        <CustomerReviews />
+      </section>
+      <section className="padding-x sm:py-32 py-16 w-full">
+        <Subscribe />
+      </section>
+      <section className=" bg-black padding-x padding-t pb-8">
+        <Footer />
+      </section>
+    </main>
+  );
 };
 
 export default App;
